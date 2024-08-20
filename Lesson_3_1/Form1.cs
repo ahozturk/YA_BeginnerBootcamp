ad7e4eea9f9daaa7786188a5a4dd68e516d055f1
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-08-20 21:14:45 +0300

    Lesson_3_1 Update

--- Lesson_3_1/Form1.cs
+++ Lesson_3_1/Form1.cs
@@ -9,16 +9,25 @@ namespace Lesson_3_1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Random random = new();
+
             for (int i = 0; i < 10; i++)
             {
+                int minStep = 10;
+                int maxStep = 25;
+
+                int horse1Step = random.Next(minStep, maxStep);
+                int horse2Step = random.Next(minStep, maxStep);
+                int horse3Step = random.Next(minStep, maxStep);
+
                 pctrBoxHorse1.Location =
-                    new Point(pctrBoxHorse1.Location.X - 20, pctrBoxHorse1.Location.Y);
+                    new Point(pctrBoxHorse1.Location.X - horse1Step, pctrBoxHorse1.Location.Y);
 
                 pctrBoxHorse2.Location =
-                    new Point(pctrBoxHorse2.Location.X - 20, pctrBoxHorse2.Location.Y);
+                    new Point(pctrBoxHorse2.Location.X - horse2Step, pctrBoxHorse2.Location.Y);
 
                 pctrBoxHorse3.Location =
-                    new Point(pctrBoxHorse3.Location.X - 20, pctrBoxHorse3.Location.Y);
+                    new Point(pctrBoxHorse3.Location.X - horse3Step, pctrBoxHorse3.Location.Y);
 
                 Thread.Sleep(100);
             }
