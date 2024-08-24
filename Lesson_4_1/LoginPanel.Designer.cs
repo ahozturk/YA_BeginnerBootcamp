132f1518f2478105f25492ba8d7a684520d1e3e5
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-08-24 15:25:56 +0300

    Lesson_4_1 Update

---
+++ Lesson_4_1/LoginPanel.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lesson_4_1
+{
+    partial class LoginPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtEmail = new TextBox();
+            btnLogin = new Button();
+            txtPassword = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(58, 153);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Font = new Font("Segoe UI", 14F);
+            txtEmail.Location = new Point(172, 153);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(177, 32);
+            txtEmail.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Segoe UI", 14F);
+            btnLogin.Location = new Point(58, 242);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(108, 45);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI", 14F);
+            txtPassword.Location = new Point(172, 191);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(177, 32);
+            txtPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16F);
+            label2.Location = new Point(58, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16F);
+            label3.Location = new Point(104, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(207, 30);
+            label3.TabIndex = 5;
+            label3.Text = "Product Hunter App";
+            // 
+            // LoginPanel
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(432, 441);
+            Controls.Add(label3);
+            Controls.Add(txtPassword);
+            Controls.Add(label2);
+            Controls.Add(btnLogin);
+            Controls.Add(txtEmail);
+            Controls.Add(label1);
+            Name = "LoginPanel";
+            Text = "LoginPanel";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtEmail;
+        private Button btnLogin;
+        private TextBox txtPassword;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Lesson_4_1/LoginPanel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_4_1
+{
+    public partial class LoginPanel : Form
+    {
+        public LoginPanel()
+        {
+            InitializeComponent();
+        }
+
+        string correctEmail = "hakan@gmail.com";
+        string correctPassword = "sifre123";
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            if (email == correctEmail && password == correctPassword)
+            {
+                MessageBox.Show("Login Success!");
+            }
+            else
+            {
+                MessageBox.Show("Error on Login!");
+            }
+        }
+    }
+}
