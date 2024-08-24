fe37e953e4cc39d7b577ff7de6bb08e7c2d2c901
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-08-24 14:39:00 +0300

    Lesson_3_1 Update

--- Lesson_3_1/Form1.Designer.cs
+++ Lesson_3_1/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             label2 = new Label();
@@ -36,6 +37,16 @@
             pctrBoxHorse2 = new PictureBox();
             pctrBoxHorse3 = new PictureBox();
             button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pctrBoxHorse1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pctrBoxHorse2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pctrBoxHorse3).BeginInit();
@@ -106,12 +117,112 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 16F);
+            button2.Location = new Point(158, 432);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 71);
+            button2.TabIndex = 7;
+            button2.Text = "Yeniden Başlat";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16F);
+            textBox1.Location = new Point(471, 413);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 36);
+            textBox1.TabIndex = 8;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 11F);
+            textBox2.Location = new Point(637, 452);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(122, 27);
+            textBox2.TabIndex = 9;
+            textBox2.Text = "15000";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 16F);
+            label4.Location = new Point(288, 416);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 30);
+            label4.TabIndex = 10;
+            label4.Text = "Speed of game:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F);
+            label5.Location = new Point(637, 432);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Horse 1:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11F);
+            label6.Location = new Point(800, 432);
+            label6.Name = "label6";
+            label6.Size = new Size(63, 20);
+            label6.TabIndex = 13;
+            label6.Text = "Horse 2:";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 11F);
+            textBox3.Location = new Point(800, 452);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(122, 27);
+            textBox3.TabIndex = 12;
+            textBox3.Text = "10000";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11F);
+            label7.Location = new Point(953, 432);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 20);
+            label7.TabIndex = 15;
+            label7.Text = "Horse 3:";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 11F);
+            textBox4.Location = new Point(953, 452);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(122, 27);
+            textBox4.TabIndex = 14;
+            textBox4.Text = "5000";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Tan;
             ClientSize = new Size(1196, 557);
+            Controls.Add(label7);
+            Controls.Add(textBox4);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(pctrBoxHorse3);
             Controls.Add(pctrBoxHorse2);
@@ -125,6 +236,7 @@
             ((System.ComponentModel.ISupportInitialize)pctrBoxHorse2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pctrBoxHorse3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -136,5 +248,15 @@
         private PictureBox pctrBoxHorse2;
         private PictureBox pctrBoxHorse3;
         private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox4;
     }
 }
--- Lesson_3_1/Form1.cs
+++ Lesson_3_1/Form1.cs
@@ -7,39 +7,109 @@
             InitializeComponent();
         }
 
+        int horse1boost = 0;
+        int horse2boost = 0;
+        int horse3boost = 0;
+
         private void button1_Click(object sender, EventArgs e)
+        {
+            int horse1Bet = Convert.ToInt32(textBox2.Text); //"15000"
+            int horse2Bet = Convert.ToInt32(textBox3.Text); //"10000"
+            int horse3Bet = Convert.ToInt32(textBox4.Text); //"5000"
+
+            int boostLevel = 2;
+
+            if (horse1Bet < horse2Bet && horse1Bet < horse3Bet)
+            {
+                // At 1 en az bahise sahiptir
+                horse1boost = boostLevel;
+                horse2boost = 0;
+                horse3boost = 0;
+            }
+            if (horse2Bet < horse1Bet && horse2Bet < horse3Bet)
+            {
+                // At 2 en az bahise sahiptir
+                horse2boost = boostLevel;
+                horse1boost = 0;
+                horse3boost = 0;
+            }
+            if (horse3Bet < horse1Bet && horse3Bet < horse2Bet)
+            {
+                // At 3 en az bahise sahiptir
+                horse3boost = boostLevel;
+                horse1boost = 0;
+                horse2boost = 0;
+            }
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
             Random random = new();
 
-            for (int i = 0; i < 10; i++)
+            int minStep = 10;
+            int maxStep = 25;
+
+            int horse1Step = random.Next(minStep + horse1boost, maxStep + horse1boost);
+            int horse2Step = random.Next(minStep + horse2boost, maxStep + horse2boost);
+            int horse3Step = random.Next(minStep + horse3boost, maxStep + horse3boost);
+
+            if (pctrBoxHorse1.Location.X > 0)
             {
-                int minStep = 10;
-                int maxStep = 25;
-
-                int horse1Step = random.Next(minStep, maxStep);
-                int horse2Step = random.Next(minStep, maxStep);
-                int horse3Step = random.Next(minStep, maxStep);
-
                 pctrBoxHorse1.Location =
-                    new Point(pctrBoxHorse1.Location.X - horse1Step, pctrBoxHorse1.Location.Y);
-
-                pctrBoxHorse2.Location =
-                    new Point(pctrBoxHorse2.Location.X - horse2Step, pctrBoxHorse2.Location.Y);
-
-                pctrBoxHorse3.Location =
-                    new Point(pctrBoxHorse3.Location.X - horse3Step, pctrBoxHorse3.Location.Y);
-
-                Thread.Sleep(100);
+                  new Point(pctrBoxHorse1.Location.X - horse1Step, pctrBoxHorse1.Location.Y);
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("At 1 Yarışı Bitirdi!");
+                return;
             }
 
-            if (pctrBoxHorse1.Location.X <= 0)
-                MessageBox.Show("At 1 Yarışı bitirdi! " + pctrBoxHorse1.Location.X);
+            if (pctrBoxHorse2.Location.X > 0)
+            {
+                pctrBoxHorse2.Location =
+                new Point(pctrBoxHorse2.Location.X - horse2Step, pctrBoxHorse2.Location.Y);
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("At 2 Yarışı Bitirdi!");
+                return;
+            }
 
-            if (pctrBoxHorse2.Location.X <= 0)
-                MessageBox.Show("At 2 Yarışı bitirdi! " + pctrBoxHorse2.Location.X);
+            if (pctrBoxHorse3.Location.X > 0)
+            {
+                pctrBoxHorse3.Location =
+                new Point(pctrBoxHorse3.Location.X - horse3Step, pctrBoxHorse3.Location.Y);
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("At 3 Yarışı Bitirdi!");
+                return;
+            }
+        }
 
-            if (pctrBoxHorse3.Location.X <= 0)
-                MessageBox.Show("At 3 Yarışı bitirdi! " + pctrBoxHorse3.Location.X);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pctrBoxHorse1.Location = new Point(1019, 12);
+            pctrBoxHorse2.Location = new Point(1019, 135);
+            pctrBoxHorse3.Location = new Point(1019, 270);
+            timer1.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string intervalText = textBox1.Text;
+            //"10" + "20" = "1020" -> string
+            //10 + 20 = 30 -> int
+
+            int interval = Convert.ToInt32(intervalText);
+
+            if (interval > 0)
+                timer1.Interval = interval;
         }
     }
 }
