784abb8ed32eb7eadfeaaffc22ebdbce11fb63b4
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-03 21:16:42 +0300

    Lesson_7_2 Update

---
+++ Lesson_7_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+
+//id,name,surname,exam1,exam2
+
+//Verini ham halini oku!
+string rawData = File.ReadAllText("C:\\Users\\Hakan\\Documents\\GitHub\\YA_BeginnerBootcamp\\Lesson_7_2\\StudentData.txt");
+
+//Okunan ham veriyi satılara böl
+string[] lines = rawData.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+//Bölünen satırları virgüllerinden ayır
+for (int i = 1; i < lines.Length; i++)
+{
+    string[] data = lines[i].Split(',');
+
+    int exam1 = Convert.ToInt32(data[3]);
+    int exam2 = Convert.ToInt32(data[4]);
+
+    Console.WriteLine($"Exam1: {exam1} - Exam2: {exam2}");
+}
