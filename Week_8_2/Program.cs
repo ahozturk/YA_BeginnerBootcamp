83a8c325cd6a295474880a272bf5617cf73967c5
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-09 21:05:54 +0300

    Lesson_8_2 Update

--- Week_8_2/Program.cs
+++ Week_8_2/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.WriteLine("Hello, World!");
 
-WriteMenu();
-WriteMenu();
-WriteMenu();
-WriteMenu();
-WriteMenu();
+//WriteMenu();
+//WriteMenu();
+//WriteMenu();
+//WriteMenu();
+//WriteMenu();
 
+//Parametre almayan, geri değer döndürmeyen
 void WriteMenu()
 {
     Console.WriteLine("Menu:");
@@ -14,3 +15,27 @@ void WriteMenu()
     //Console.WriteLine("3-Contact Us");
     Console.WriteLine("3-Exit");
 }
+
+//Parametre almayan, geri değer döndüren
+double GetPi()
+{
+    return 3.14;
+}
+
+double r = 3;
+double calculation = 3 * 2 * GetPi();
+
+//Parametre alan, geri değer döndürmeyen
+void WriteMenuWithName(string name)
+{
+    Console.WriteLine($"Hello {name}");
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1-Login");
+    Console.WriteLine("2-Register");
+    //Console.WriteLine("3-Contact Us");
+    Console.WriteLine("3-Exit");
+}
+
+WriteMenuWithName("Merve");
+
+//Parametre alan, geri değer döndüren
