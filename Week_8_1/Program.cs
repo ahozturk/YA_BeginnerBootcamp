afe8b218e0ab7a7415aac61a8eca0978d7254f50
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-09 20:19:05 +0300

    Lesson_8_1 Update

---
+++ Week_8_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!");
+
+//5!
+//5*4*3*2
+
+//3!
+//3*2
+
+Console.WriteLine("Faktoriyel hesaplamak için sayı giriniz:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int total = 1;
+
+for (int i = userNumber; i > 1; i--)
+{
+    total = total * i;
+}
+
+Console.WriteLine($"{userNumber} faktoriyel = {total}");
