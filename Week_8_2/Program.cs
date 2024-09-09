a7db0af0d2ad285c88819775721358e7131d9e76
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-09 20:55:24 +0300

    Lesson_8_2 Update

---
+++ Week_8_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+
+WriteMenu();
+WriteMenu();
+WriteMenu();
+WriteMenu();
+WriteMenu();
+
+void WriteMenu()
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1-Login");
+    Console.WriteLine("2-Register");
+    //Console.WriteLine("3-Contact Us");
+    Console.WriteLine("3-Exit");
+}
