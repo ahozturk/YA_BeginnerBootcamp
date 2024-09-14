3087c3ec7843ef58787c1dbb4030627f4081e215
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-14 14:40:09 +0300

    Lesson_9_1 Update

--- Week_9_1/Program.cs
+++ Week_9_1/Program.cs
@@ -7,9 +7,11 @@ List<string> products = new(); //"Eraser", "Pen"
 
 while (true)
 {
-    Console.WriteLine("1-List Products\n2-Add Product");
+    //Console.WriteLine("1-List Products\n2-Add Product");
+    //Console.Write("User: ");
+    //string selection = Console.ReadLine();
 
-    string selection = Console.ReadLine();
+    string selection = TakeInputFromUser("1-List Products\n2-Add Product");
 
     if (selection == "1")
     {
@@ -21,9 +23,28 @@ while (true)
 
     else if (selection == "2")
     {
-        Console.WriteLine("Please Enter Product Name:");
-        string newProduct = Console.ReadLine();
+        //Console.WriteLine("Please Enter Product Name:");
+        //Console.Write("User: ");
+        //string newProduct = Console.ReadLine();
+
+        string newProduct = TakeInputFromUser("Please Enter Product Name:");
 
         products.Add(newProduct);
     }
+    SeparateLines();
+}
+
+void SeparateLines()
+{
+    Console.WriteLine("-----------------");
+    //Console.WriteLine("\n\n\n");
+}
+
+string TakeInputFromUser(string message)
+{
+    Console.WriteLine(message);
+    Console.Write("user: ");
+    string input = Console.ReadLine();
+
+    return input;
 }
