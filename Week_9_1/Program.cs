d57f023168847fe75f4d8561b8f5a4171f504cb3
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-14 15:33:20 +0300

    Lesson_9_1 Update

--- Week_9_1/Program.cs
+++ Week_9_1/Program.cs
@@ -3,7 +3,10 @@
 //List
 //Add
 
-List<string> products = new(); //"Eraser", "Pen"
+//ProductToUpdate (Şu anki hali) -> Eraaser
+//UpdatedProduct (Yeni hali) -> Eraser
+
+List<string> products = LoadProductsFromCSV(); //"Eraaser", "Pen"
 
 while (true)
 {
@@ -11,7 +14,9 @@ while (true)
     //Console.Write("User: ");
     //string selection = Console.ReadLine();
 
-    string selection = TakeInputFromUser("1-List Products\n2-Add Product");
+    string selection = TakeInputFromUser("1-List Products\n2-Add Product\n3-Delete\n4-Update");
+
+    //2
 
     if (selection == "1")
     {
@@ -31,6 +36,44 @@ while (true)
 
         products.Add(newProduct);
     }
+
+    else if (selection == "3")
+    {
+        string deleteProduct = TakeInputFromUser("Please Enter Product Name to Delete:");
+
+        bool isDeleted = products.Remove(deleteProduct);
+
+        if (isDeleted)
+            Console.WriteLine($"{deleteProduct} is deleted!");
+        else
+            Console.WriteLine("Product Not Found!");
+
+    }
+
+    else if (selection == "4")
+    {
+        string productToUpdate = TakeInputFromUser("Please Enter Product Name to Update:"); //"Eraaser"
+
+        string updatedProduct = TakeInputFromUser("Please Enter Product Name to Update:"); //"Eraser"
+
+        int index = products.IndexOf(productToUpdate); //0
+
+        if (index == -1)
+        {
+            Console.WriteLine("Product Not Found!");
+            continue;
+        }
+
+        products[index] = updatedProduct;
+
+        //products[1] = "Pencil"; //Update on Lists
+
+        Console.WriteLine($"{productToUpdate} is updated as {updatedProduct}!");
+    }
+
+    //3-Delete
+    //4-Update
+    SaveProductsToCSV(products);
     SeparateLines();
 }
 
@@ -47,4 +90,23 @@ string TakeInputFromUser(string message)
     string input = Console.ReadLine();
 
     return input;
+}
+
+// Method to save products to a CSV file
+void SaveProductsToCSV(List<string> products)
+{
+    File.WriteAllLines("products.csv", products);
+}
+
+// Method to load products from a CSV file
+List<string> LoadProductsFromCSV()
+{
+    if (File.Exists("products.csv"))
+    {
+        return File.ReadAllLines("products.csv").ToList();
+    }
+    else
+    {
+        return new List<string>();
+    }
 }
