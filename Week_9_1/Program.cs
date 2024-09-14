6e1d406f9677fe5303c8c8fb15969f9e30bd8c7c
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-09-14 14:27:12 +0300

    Lesson_9_1 Update

---
+++ Week_9_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Yazılım Academy - Example Project!");
+
+//List
+//Add
+
+List<string> products = new(); //"Eraser", "Pen"
+
+while (true)
+{
+    Console.WriteLine("1-List Products\n2-Add Product");
+
+    string selection = Console.ReadLine();
+
+    if (selection == "1")
+    {
+        for (int i = 0; i < products.Count; i++)
+        {
+            Console.WriteLine(products[i]);
+        }
+    }
+
+    else if (selection == "2")
+    {
+        Console.WriteLine("Please Enter Product Name:");
+        string newProduct = Console.ReadLine();
+
+        products.Add(newProduct);
+    }
+}
